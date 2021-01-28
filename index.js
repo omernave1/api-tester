8a90501e5620841480c88828999452ddd0102549
Omer Nave <omer.nave@vonage.com>
2021-01-28 16:29:28 +0200

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+import { templates } from "./src/actions/tests";
+
+function run() {
+    console.log('hello');
+}
+
+// function addTestTemplates(key, value) {
+//     if (!templates[key]) {
+//         templates[key] = value;
+//     } else {
+//         throw new Error('Temp with this name already exists')
+//     }
+// }
+//
+// function getTestTemplates() {
+//     return templates;
+// }
+
+module.exports = {
+    run: run,
+    templates
+};
